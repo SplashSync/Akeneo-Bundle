3284bdcc22e1581a623407d853734b39a99e33d3
BadPixxel <eshop.bpaquier@gmail.com>
2019-10-22 02:32:46 +0200

    WIP: Migrate to Splash V2

--- tests/KernelTestCase.php
+++ tests/KernelTestCase.php
@@ -43,6 +43,12 @@ class TestCase extends BaseTestCase
     protected function setUp()
     {
         //====================================================================//
+        // Force Debug Mode
+        if (!defined('SPLASH_DEBUG')) {
+            define('SPLASH_DEBUG', true);
+        }
+
+        //====================================================================//
         // Boot Symfony Kernel
         /** @var ContainerInterface $container */
         $container = static::bootKernel()->getContainer();
