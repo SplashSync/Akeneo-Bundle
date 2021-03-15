97426cdb57fda6f46bb541edcb3a3ad2f97fc654
BadPixxel <eshop.bpaquier@gmail.com>
2021-03-15 19:34:32 +0100

    WIP: Gitlab CI

---
+++ ci/install_akeneo.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+################################################################################
+#
+# Copyright (C) 2021 BadPixxel <www.badpixxel.com>
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+################################################################################
+
+echo "----------------------------------------------------"
+echo "--> AKENEO - Install Version $AKENEO_VERSION"
+echo "----------------------------------------------------"
+
+echo "Create Akeneo Community Project"
+composer self-update --1
+composer create-project akeneo/pim-community-dev akeneo $AKENEO_VERSION --prefer-dist --no-plugins
+
+echo "Configuring Akeneo"
+cp travis/parameters.yml.dist akeneo/app/config/parameters.yml
+cp travis/parameters.yml.dist akeneo/app/config/parameters_test.yml
+
+echo "Run Akeneo Installer"
+cd akeneo
+chmod -x bin/console
+php bin/console pim:install               --env=prod  --force --symlink --clean --no-interaction --no-debug
+php bin/console pim:installer:assets      --env=prod  --symlink --clean --no-interaction --no-debug
---
+++ ci/install_splash.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+################################################################################
+#
+# Copyright (C) 2021 BadPixxel <www.badpixxel.com>
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+################################################################################
+
+echo "----------------------------------------------------"
+echo "--> AKENEO - Install Splash Bundle"
+echo "----------------------------------------------------"
+
+echo "Push Configs for Akeneo"
+cat   ../travis/routing_splash.yml    >> app/config/routing.yml
+cat   ../travis/config_splash.yml     >> app/config/config.yml
+cp    ../phpunit.xml.dist phpunit.xml.dist
+
+echo "Register Symfony Bundles"
+sed -i 's|// your app bundles should be registered here|new \\Splash\\Bundle\\SplashBundle(), new Splash\\Akeneo\\SplashAkeneoBundle(),|g' app/AppKernel.php
+#cat app/AppKernel.php
+
+echo "Composer Require"
+composer require splash/phpcore:dev-master splash/php-bundle:dev-master splash/akeneo-bundle:dev-master --no-interaction --prefer-dist --no-suggest
+
+mkdir  -p ./src/Splash/Tests/Tools
+cp    ../tests/KernelTestCase.php ./src/Splash/Tests/Tools/TestCase.php
+cp    ../tests/KernelTestCase.php ./vendor/splash/phpcore/Tests/Tools/TestCase.php
+
+echo "Splash Bundle Configuration"
+php bin/console debug:config splash       --env=test
