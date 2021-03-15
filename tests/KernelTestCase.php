2c43095326ad605c3f96e84cd13ca14a0fc322f4
BadPixxel <eshop.bpaquier@gmail.com>
2021-03-15 14:01:59 +0100

    FIX: Catalog Mode for Metrics

--- tests/KernelTestCase.php
+++ tests/KernelTestCase.php
@@ -33,7 +33,6 @@ class TestCase extends BaseTestCase
     use \Splash\Bundle\Tests\ConnectorAssertTrait;
     use \Splash\Bundle\Tests\ConnectorTestTrait;
     use \Splash\Tests\Tools\Traits\ObjectsAssertionsTrait;
-    use \Splash\Tests\Tools\Traits\SuccessfulTestPHP7;
 
     /**
      * Boot Symfony & Setup First Server Connector For Testing
