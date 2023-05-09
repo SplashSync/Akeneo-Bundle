cabad3fca805e4e5a29d97b76c436e8f4b68610e
B. Paquier <eshop.bpaquier@gmail.com>
2023-05-09 21:00:34 +0000

    FIX: CI

--- ci/install_splash.sh
+++ ci/install_splash.sh
@@ -31,7 +31,7 @@ echo "Register Symfony Bundles"
 cp   ../ci/bundles.test.php       config/bundles.test.php
 
 echo "Composer Require"
-composer require splash/akeneo-bundle:dev-master -W --no-interaction --prefer-dist --no-scripts --no-plugins
+composer require splash/akeneo-bundle:dev-master --with-all-dependencies --no-interaction --prefer-dist --no-scripts --no-plugins
 cp  -Rf ../src/       vendor/splash/akeneo-bundle/src/
 ls  -l vendor/splash/akeneo-bundle/src/
 
