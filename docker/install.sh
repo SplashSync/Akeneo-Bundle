34bb51a7385cebc80d7e3609ba221bc411d67c1d
BadPixxel <eshop.bpaquier@gmail.com>
2025-04-04 11:29:59 +0200

    Switch to `docker compose` commands over `docker-compose`
    Replaced all occurrences of the deprecated `docker-compose` with the updated `docker compose` syntax for compatibility with newer versions. This change ensures better maintainability and aligns with official Docker recommendations.

--- docker/install.sh
+++ docker/install.sh
@@ -19,34 +19,34 @@ echo -e "\e[45m *********************************** \e[49m"
 echo -e "\e[45m ** Build Akeneo Docker ...          \e[49m"
 echo -e "\e[45m *********************************** \e[49m"
 
-docker-compose up -d
+docker compose up -d
 
 echo -e "\e[45m *********************************** \e[49m"
 echo -e "\e[45m ** Install Akeneo ...               \e[49m"
 echo -e "\e[45m *********************************** \e[49m"
 
 echo -e "\e[45m ** Composer Update                  \e[49m"
-docker-compose exec fpm composer update
-#docker-compose exec fpm composer install
+docker compose exec fpm composer update
+#docker compose exec fpm composer install
 
 echo -e "\e[45m ** Yarn Install                     \e[49m"
-docker-compose run --rm node yarn install
-docker-compose run --rm node yarn upgrade
+docker compose run --rm node yarn install
+docker compose run --rm node yarn upgrade
 
 echo -e "\e[45m ** Akeneo Install                   \e[49m"
-docker-compose exec fpm chmod -x bin/console
-docker-compose exec fpm chmod 777 bin/console
-docker-compose exec fpm php bin/console --env=prod cache:clear --no-warmup
-docker-compose exec fpm php bin/console --env=prod pim:install:db \
+docker compose exec fpm chmod -x bin/console
+docker compose exec fpm chmod 777 bin/console
+docker compose exec fpm php bin/console --env=prod cache:clear --no-warmup
+docker compose exec fpm php bin/console --env=prod pim:install:db \
               --catalog=vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal
 #              --catalog=vendor/akeneo/pim-community-dev/src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/icecat_demo_dev
-docker-compose exec fpm php bin/console --env=prod pim:installer:assets --symlink --clean
+docker compose exec fpm php bin/console --env=prod pim:installer:assets --symlink --clean
 
 echo -e "\e[45m ** Webpack Install                   \e[49m"
-docker-compose run --rm node yarn run webpack
+docker compose run --rm node yarn run webpack
 
 echo -e "\e[45m ** Install Php Extensions ...       \e[49m"
-docker-compose exec fpm apt update
-docker-compose exec fpm apt install php-soap
-docker-compose restart fpm
+docker compose exec fpm apt update
+docker compose exec fpm apt install php-soap
+docker compose restart fpm
 
